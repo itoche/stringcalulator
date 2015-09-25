f35825824124f61c290f578b724eb49a8d3966ff
itoche <VWwby9T0>
2015-09-25 11:28:22 +0200

    Requirement 2: For an empty string the method will return 0

--- src/com/arhs/spikeseed/tdd/stringcalculator/StringCalculator.java
+++ src/com/arhs/spikeseed/tdd/stringcalculator/StringCalculator.java
@@ -7,18 +7,22 @@ public class StringCalculator {
 
     /**
      * Create a simple String calculator with a method int Add(string numbers)
-     * The method can take 0, 1 or 2 numbers, and will return their sum (for an empty string it will return 0) for example “” or “1” or “1,2”
+     * Requirement 1: The method can take 0, 1 or 2 numbers, and will return their sum (for an empty string it will return 0) for example “” or “1” or “1,2”
+     * Requirement 2: For an empty string the method will return 0
      *
      * @param numbers
      */
-    public static final void add(final String numbers) {
+    public static final int add(final String numbers) { // Changed void to int
         String[] numbersArray = numbers.split(",");
         if (numbersArray.length > 2) {
             throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
         } else {
             for (String number : numbersArray) {
-                Integer.parseInt(number); // If it is not a number, parseInt will throw an exception
+                if (!number.isEmpty()) {
+                    Integer.parseInt(number);
+                }
             }
         }
+        return 0; // Added return
     }
 }
--- test/com/arhs/spikeseed/tdd/stringcalculator/StringCalculatorTest.java
+++ test/com/arhs/spikeseed/tdd/stringcalculator/StringCalculatorTest.java
@@ -8,6 +8,11 @@ import org.junit.Test;
  */
 public class StringCalculatorTest {
 
+    //
+    // Create a simple String calculator with a method int Add(string numbers)
+    // Requirement 1: The method can take 0, 1 or 2 numbers, and will return their sum (for an empty string it will return 0) for example “” or “1” or “1,2”
+
+
     @Test(expected = RuntimeException.class)
     public final void whenMoreThan2NumbersAreUsedThenExceptionIsThrown() {
         StringCalculator.add("1,2,3");
@@ -20,5 +25,11 @@ public class StringCalculatorTest {
     @Test(expected = RuntimeException.class)
     public final void whenNonNumberIsUsedThenExceptionIsThrown() {
         StringCalculator.add("1,X");
+    }
+
+    // Requirement 2: For an empty string the method will return 0
+    @Test
+    public final void whenEmptyStringIsUsedThenReturnValueIs0() {
+        Assert.assertEquals(0, StringCalculator.add(""));
     }
 }
